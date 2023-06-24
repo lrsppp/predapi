423d976792b9bf8c6d23f1dae27d5384b68fa3f1
Lars Poppe <p.larsf@gmail.com>
2023-06-24 17:51:45 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='predapi',
+    version='0.1',
+    description="API for CNN for Image Classification",
+    packages=find_packages(),
+)
