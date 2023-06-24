95c2be7d89ab448283797e06f10df8cad0b610c8
Lars Poppe <p.larsf@gmail.com>
2023-06-24 17:53:16 +0200

    update env, mv config models to models

--- predapi/cnn.py
+++ predapi/cnn.py
@@ -1,39 +1,6 @@
-import numpy as np
-from tensorflow.keras.utils import to_categorical
-from pydantic import BaseSettings
-
-from sklearn.model_selection import train_test_split
-
+from predapi.models import ModelConfig, TrainConfig
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
-
-
-def load_data(file_path, test_size=0.2, random_state=42):
-    X, y = np.load(file_path, allow_pickle=True).T
-
-    X = np.asarray([x.reshape(20, 20) for x in X], dtype=np.float32)
-    y = np.asarray(y, dtype=np.int32)
-    y = to_categorical(y)
-
-    train_data, test_data, train_labels, test_labels = train_test_split(
-        X, y, test_size=test_size, random_state=random_state
-    )
-
-    val_data, test_data, val_labels, test_labels = train_test_split(
-        test_data, test_labels, test_size=0.5, random_state=42
-    )
-
-    return train_data, train_labels, test_data, test_labels, val_data, val_labels
-
-
-class ModelConfig(BaseSettings):
-    num_filters: int
-    filter_size: int
-    pool_size: int
-
-
-class TrainConfig(BaseSettings):
-    epochs: int
 
 
 class SimpleCNN:
@@ -79,29 +46,3 @@ class SimpleCNN:
     def save_model(self, file_path):
         self.model.save(file_path)
 
-
-def main():
-    config = ModelConfig(num_filters=8, filter_size=2, pool_size=2)
-    train_config = TrainConfig(
-        epochs=50,
-    )
-    cnn_object = SimpleCNN(config=config)
-
-    train_data, train_labels, test_data, test_labels, val_data, val_labels = load_data(
-        "data.npy"
-    )
-
-    cnn_object.train(
-        train_data, train_labels, val_data, val_labels, config=train_config
-    )
-
-    accuracy = cnn_object.model.evaluate(test_data, test_labels)[1]
-    print("Test accuracy:", accuracy)
-
-    # write to file
-    model_file_path = "model.h5"
-    cnn_object.save_model(model_file_path)
-
-
-if __name__ == "__main__":
-    main()
--- predapi/models.py
+++ predapi/models.py
@@ -1,5 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, BaseSettings
 from typing import List, Optional
+
+
+class ModelConfig(BaseSettings):
+    num_filters: int
+    filter_size: int
+    pool_size: int
+
+
+class TrainConfig(BaseSettings):
+    epochs: int
 
 
 class Signal2D(BaseModel):
